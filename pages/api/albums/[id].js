cbd3b5f19b2f1408a1380fb7e40c91dab9fd0c54
Guilherme Eiras <guialbertoeiras@hotmail.com>
2022-08-20 09:49:38 -0300

    feat(albums): add update endpoint

--- pages/api/albums/[id].js
+++ pages/api/albums/[id].js
@@ -3,6 +3,7 @@ import { ObjectId } from 'mongodb'
 
 export default (req, res) => {
   if (req.method === 'GET') { return show(req, res) }
+  if (req.method === 'POST') { return update(req, res) }
 
   return res.status(404).send({ error: 'Route not found' })
 }
@@ -20,5 +21,25 @@ async function show (req, res) {
     }
   } catch (error) {
     res.status(422).json(error);
+  }
+}
+
+async function update (req, res) {
+  const stickers = req.body.stickers;
+
+  if (stickers) {
+    const updateParams = Object
+      .keys(stickers)
+      .reduce((memo, stickNumber) => {
+        memo[`stickers.${stickNumber}`] = stickers[stickNumber];
+        return memo;
+      }, {})
+
+    const albums = database.get('albums')
+    await albums.update({ _id: ObjectId(req.query.id) }, { $set: updateParams })
+
+    return show(req, res)
+  } else {
+    res.status(422).json({});
   }
 }
